9de600d392738ba5fa58d3903fb0e1829e9c43af
koungkmitl <59070025@it.kmitl.ac.th>
2018-02-17 16:54:34 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,57 @@
+const config = {
+    apiKey: "<KEY>",
+    authDomain: "<KEY>",
+    databaseURL: "<KEY>",
+    projectId: "<KEY>",
+    storageBucket: "<KEY>",
+    messagingSenderId: "<KEY>"
+};
+firebase.initializeApp(config);
+
+const analogLed = firebase.database().ref('analog');
+const resistorLed = firebase.database().ref('history');
+const digitalLed = firebase.database().ref('digital');
+let text = document.getElementById("text-btn");
+
+function submitBtn1() {
+    analogLed.set(text.value);
+}
+
+function submitBtn2() {
+    resistorLed.push(text.value);
+}
+
+function ratioBtnOn() {
+    digitalLed.set("on");
+}
+
+function ratioBtnOff() {
+    digitalLed.set('off');
+}
+
+const chart = Highcharts.chart('container', {
+    title: {
+        text: 'Level LED'
+    },
+    xAxis: {
+        tickInterval: 1
+    },
+    yAxis: {
+        type: 'logarithmic',
+        minorTickInterval: 1
+    },
+    tooltip: {
+        headerFormat: '<b></b><br />',
+        pointFormat: 'x = {point.x}, y = {point.y}'
+    },    
+    series: [{
+        id: 0,
+        name: 'LED',
+        data: []
+    }]
+});
+
+resistorLed.on('value', snapshot => {
+    const points = Object.values(snapshot.val()).map(parseFloat);
+    chart.series[0].setData(points, true);
+})
